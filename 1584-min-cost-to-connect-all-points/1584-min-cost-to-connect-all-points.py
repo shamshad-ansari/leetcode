7d34fcf008bf39185b2e84a4f3706c85236244d3
Shamshad Ansari <149064103+shamshad-ansari@users.noreply.github.com>
2026-07-09 14:06:20 -0500

    Time: 1160 ms (47.66%), Space: 158.5 MB (17.79%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,58 +1,38 @@
-# Will do with Prim's later
-class UnionFind:
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.rank = [1] * n
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])   # Path compression
-        return self.parent[x]
-
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-
-        if rootX == rootY:
-            return False
-
-        # Union by rank
-        if self.rank[rootX] > self.rank[rootY]:
-            self.parent[rootY] = rootX
-        elif self.rank[rootX] < self.rank[rootY]:
-            self.parent[rootX] = rootY
-        else:
-            self.parent[rootY] = rootX
-            self.rank[rootX] += 1
-
-        return True
-
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
+        visited = [False] * n
 
-        # Build every edge
-        edges = []
+        adj = [[] for _ in range(n)]
+
         for i in range(n):
             x1, y1 = points[i]
             for j in range(i + 1, n):
                 x2, y2 = points[j]
                 dist = abs(x1 - x2) + abs(y1 - y2)
-                edges.append((dist, i, j))
 
-        # Sort edges by weight
-        edges.sort()
+                adj[i].append((j, dist))
+                adj[j].append((i, dist))
 
-        uf = UnionFind(n)
         total = 0
-        edges_used = 0
+        visited_count = 0
+        pq = [(0, 0)]          # (weight, node)
 
-        for dist, u, v in edges:
-            if uf.union(u, v):
-                total += dist
-                edges_used += 1
+        while pq:
+            wt, node = heapq.heappop(pq)
 
-                if edges_used == n - 1:
-                    break
+            if visited[node]:
+                continue
+
+            visited[node] = True
+            total += wt
+            visited_count += 1
+
+            if visited_count == n:
+                break
+
+            for neighbor, edge_wt in adj[node]:
+                if not visited[neighbor]:
+                    heapq.heappush(pq, (edge_wt, neighbor))
 
         return total
