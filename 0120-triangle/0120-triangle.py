6b1b7e4af4121453538136bfe6c3457920887364
Shamshad Ansari <149064103+shamshad-ansari@users.noreply.github.com>
2026-09-01 22:10:10 -0500

    Time: 15 ms (5.74%), Space: 23.3 MB (8.62%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,13 +1,17 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        @cache
+        memo = {}
         def solve(row,col):
             if row >= len(triangle) or col >= len(triangle[-1]):
                 return 0
             
-            left= triangle[row][col] + solve(row+1,col)
+            if (row,col) in memo:
+                return memo[(row,col)]
+            
+            left = triangle[row][col] + solve(row+1,col)
             right = triangle[row][col] + solve(row+1,col+1)
 
-            return min(left, right)
+            memo[(row,col)] = min(left, right)
+            return memo[(row,col)]
 
-        return solve(0,0)        
+        return solve(0,0)
